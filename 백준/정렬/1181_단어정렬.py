1dd578249de0267b1e073b1f1f23d1bfc9416f27
singga <qwerewqaa@naver.com>
2025-11-27 16:24:45 +0900

    create 1181

---
+++ 백준/정렬/1181_단어정렬.py
@@ -0,0 +1,16 @@
+import sys
+from collections import Counter
+
+input = sys.stdin.readline
+
+n = int(input().strip())
+
+arr = []
+for i in range(n):
+    s = input().strip()
+    if s not in arr:
+        arr.append(s)
+
+arr.sort(key=lambda x:(len(x), x))
+for i in arr:
+    print(i)
