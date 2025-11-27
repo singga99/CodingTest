ccc3949d2c36f70792cd8733d0bc13dd04fc8619
singga <qwerewqaa@naver.com>
2025-11-27 15:52:51 +0900

    update 10989

--- 백준/정렬/10989_수정렬하기3.py
+++ 백준/정렬/10989_수정렬하기3.py
@@ -1,4 +1,22 @@
+'''
+[ 계수 정렬(Counting sort) ]
+
+배열에 존재하는 수 개수 세고, 이를 바탕으로 정렬 수행
+
+
+'''
+
 import sys
+from collections import Counter
 
 input = sys.stdin.readline
 
+n = int(input())
+counter = Counter()
+
+for _ in range(n):
+    counter[int(input())] += 1
+
+for number in sorted(counter):
+    for _ in range(counter[number]):
+        print(str(number))
