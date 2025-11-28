810005cfa85ae524647a04eb32e48d3e04d58341
singga <qwerewqaa@naver.com>
2025-11-28 15:13:06 +0900

    update 18870

--- 백준/정렬/18870_좌표압축.py
+++ 백준/정렬/18870_좌표압축.py
@@ -3,3 +3,12 @@ import sys
 input = sys.stdin.readline
 n = int(input().strip())
 
+arr = list(map(int, input().strip().split()))
+sort_arr = sorted(set(arr))
+index_dic = {}
+
+for i in range(len(sort_arr)):
+    index_dic[sort_arr[i]] = i
+
+for i in arr:
+    print(index_dic[i], end=' ')
