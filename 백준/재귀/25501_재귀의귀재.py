958f685b077a0b6289dd1b16a77923671f452dac
singga <qwerewqaa@naver.com>
2025-12-11 13:47:01 +0900

    create 25501

---
+++ 백준/재귀/25501_재귀의귀재.py
@@ -0,0 +1,22 @@
+import sys
+
+input = sys.stdin.readline
+
+def recursion(s, l, r):
+    global cnt
+    cnt += 1
+    if l >= r: return 1
+    elif s[l] != s[r]: return 0
+    else: return recursion(s, l+1, r-1)
+
+def isPalindrome(s):
+    return recursion(s, 0, len(s)-1)
+
+t = int(input().strip())
+
+for _ in range(t):
+    s = input().strip()
+    
+    cnt = 0
+    result = isPalindrome(s)
+    print(result, cnt)
